bed8002f6f12f5eb16cf59dccd029a343c2d5b21
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-08 15:06:58 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main -Entry point
- * Description - prints a sentence
- * Return:0
+ *
+ * Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like buliding a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
