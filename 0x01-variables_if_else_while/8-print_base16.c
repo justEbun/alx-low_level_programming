2075abf334c8cdb8f829c434ad0a5fea999111d7
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-09 11:03:31 -0700

    some new stuff

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9' ; i++)
+	{
+		putchar(i);
+
+		if (i != '9')
+			putchar(',');
+			putchar(' ');		
+	}
+	return (0);
+}
