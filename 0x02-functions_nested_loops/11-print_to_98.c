3b114eaf26533e6c6651830384ac65e77d17dab8
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-13 08:47:26 -0700

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
@@ -24,5 +25,5 @@ void print_to_98(int n)
 			n--;
 		}
 	}
-	printf("98\n");
+	printf("98\n";
 }
