b857f1eb19e3214d6453fcde5e28fa08a9eda41f
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-13 04:05:53 -0700

    some new files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+	for (alpha = 'a';alpha <= 'z'; alpha++)
+	{
+		putchar (alpha);
+		putchar ('\n');
+	}
+
+	int main(void)
+	{
+		print_alphabet();
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+    	char alpha;
+        int num = 0;
+	while(num <= 9)
+       {
+	        for (alpha = 'a';alpha <= 'z';alpha++)
+		        {
+				putchar(alpha);
+				putchar('\n');
+		        }
+		        num++;
+	}
+}
+
+int main(void)
+{
+	    print_alphabet_x10();
+	    return 0;
+}
