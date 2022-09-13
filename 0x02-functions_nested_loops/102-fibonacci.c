3455151b36a22487a02b82fb2bef91c72422bcea
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-13 13:28:13 -0700

    first 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - to check the code
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	int i = 0;
+	long l = 1, m = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+		{
+			printf("%ld", l);
+		}
+		else if (i == 1)
+		{
+			printf(", %ld", m);
+		}
+		else 
+		{
+			m += l;
+			l = m - l;
+			printf(", %ld",m);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
