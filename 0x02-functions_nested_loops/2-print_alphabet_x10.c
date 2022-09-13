8cc8fdae42482d0e5470d89ef526b726e1af72fc
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-13 06:06:44 -0700

    add

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: void
  */
 
 void print_alphabet_x10(void)
@@ -19,5 +19,4 @@ void print_alphabet_x10(void)
 		        num++;
 			_putchar('\n');
 	}
-	return (0);
 }
