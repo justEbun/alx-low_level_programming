6665fe2c2ef760a2f463fe806c5423792c0dbc0b
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-13 06:12:20 -0700

    add

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@
 
 void print_alphabet_x10(void)
 {
-    	char c; 
-        int num = 0;
+	char c;
+	int num = 0;
 	while(num <= 9)
-       {
-	        for (c = 'a';c <= 'z';c++)
-		        {
-				_putchar(c);
-		        }
-		        num++;
-			_putchar('\n');
+	{
+		for (c = 'a';c <= 'z';c++)
+		{
+			_putchar(c);
+		}
+		num++;
+		_putchar('\n');
 	}
 }
