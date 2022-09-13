a82abf121c8ea82205cdc5c7116c0cbe5ed683ad
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-13 16:44:05 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - to run the code
+ *
+ * return:0
+ */
+
+int main(void)
+{
+	int a = 0, b = 1, n = 0, s = 0;
+
+	while (n < 4000000)
+	{
+		n = a + b;
+		a = b;
+		b = n;
+		if (n % 2 == 0)
+			s += n;
+	}
+	printf("%d\n", s);
+	return (0);
+}
