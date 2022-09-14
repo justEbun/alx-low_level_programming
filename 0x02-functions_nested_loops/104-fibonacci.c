c0a5db95d42feddd36f2a43cf6681d76e2c1367d
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-13 18:14:00 -0700

    Corrected

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,6 +9,7 @@
 int _fibonacci(int i)
 {
 	int l = 0;
+
 	if (!i)
 	{
 		return (1);
@@ -24,14 +25,14 @@ int _fibonacci(int i)
 
 int main(void)
 {
-	unsigned long f1 = 1, f2 =2, tmp, mx = 100000000, f1o =0, f2o =, tmp = 0;
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
 	short int n = 1, in;
 
 	while (n <= 98)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
-		in = _fibonacci(mx) - 1 -_fibonacci(f1);
+		in = _fibonacci(mx) - 1 - _fibonacci(f1);
 		while (f1o > 0 && in > 0)
 		{
 			printf("%d", 0);
