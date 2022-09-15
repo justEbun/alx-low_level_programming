9c3f578faede4ee6822bdaf8de91466a5e6a3a63
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-15 05:11:53 -0700

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number 0 to 9
+ * return: 0
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i);
+		_putchar('\n');
+		i++;
+	}
+}
