11af70e56938b05771a97953ef259a6076988961
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-15 07:09:14 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: the integer value
+ * return: void
+ */
+
+void print_square(int size)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < size; i++)
+	{
+		for(c = 0; c < size; c++)
+		{
+			purchar('#');
+		}
+		putchar('/n');
+	}
+}
