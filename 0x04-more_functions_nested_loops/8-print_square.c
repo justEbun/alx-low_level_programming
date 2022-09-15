d1f6f729e28c11140b5b8a203405543dec4dbb3b
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-15 09:10:07 -0700

    corrected

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,19 +11,12 @@ void print_square(int size)
 	int i;
 	int c;
 
-	if (size > 0)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		for (c = 0; c < size; c++)
 		{
-			for (c = 0; c < size; c++)
-			{
-				_purchar('#');
-			}
-			_putchar('/n');
+			_purchar('#');
 		}
-	}
-	else
-	{
 		_putchar('/n');
 	}
 }
