65afa038a685486187e7243611a224e9e7f647ba
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-15 06:05:50 -0700

    prints a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: the integer value
+ * return: void
+ */
+
+void print_line(int n);
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
