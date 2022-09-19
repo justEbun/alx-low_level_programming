a60154a4c34a3b752edacf5f4ea12f165aa4b788
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-19 12:04:28 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str: thr character value
+ * return: 0
+ */
+
+void puts_half(char *str)
+{
+	int s;
+
+	for (s = 0; str[s] !1 = '\0'; s++)
+		;
+	for (s /= 2; str[s] != '\0'; s++)
+	{
+		_putchar(str[s]);
+	}
+	_putchar('\n');
+}
