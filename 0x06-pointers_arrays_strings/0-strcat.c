50f4238c2173888ebb42fd6f0ac3df369f6eca75
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-21 14:17:16 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,7 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i, j = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,7 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i, j = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
