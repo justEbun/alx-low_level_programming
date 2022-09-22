7334eb4f5cb0b22523165e42d914ed9ac014fa64
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-22 10:14:53 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * *_strcmp - compares two strings
+ * _strcmp - compares two strings
  * @s1: character value
  * @s2: character value
  * Return: 0
  */
 
-char *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	while ((*s1 && *s2) && (*s1 == *s2))
 	{
