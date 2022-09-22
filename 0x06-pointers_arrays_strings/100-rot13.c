8c675664363e993cd57e551ebdacffee20051b70
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-22 14:12:51 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: string
+ * Return: 0
+ */
+
+char *rot13(char *str)
+{
+	int i1, i2;
+	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char rot13[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	while (str[i1])
+	{
+		for (i2 = 0; i2 < 52; i2++)
+		{
+			if (str[i1] == alpha[i2])
+			{
+				str[i1] = rot13[i2];
+				break;
+			}
+		}
+		i1++;
+	}
+	return (str);
+}
