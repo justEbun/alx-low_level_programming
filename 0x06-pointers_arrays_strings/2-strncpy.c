7c58aa0ece9e9b352d8a75a4742817ac4394b7c9
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-22 06:48:59 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ * @n: the integer value
+ * Return: the destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (src[i++])
+		j++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[i++] = src[i];
+	for (i = j; i < n; i++)
+		dest[i] = '\0';
+	
+	return (dest);
+}
