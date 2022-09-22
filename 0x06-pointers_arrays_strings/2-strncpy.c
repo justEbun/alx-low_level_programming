805006d173bfa9516a23bd51f65a2ffeb0cb72de
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-22 15:30:07 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
 
-	while (src[i++])
-		j++;
-	for (i = 0; src[i] && i < n; i++)
-		dest[i++] = src[i];
-	for (i = j; i < n; i++)
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
 		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
