9433a73da6b101342107d52b9dc0e28f1cc6204c
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-26 16:10:24 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer
+ * @accept: pointer
+ * Return: count
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+
+	for (int i = 0; accept[i] != '\0'; i++)
+	{
+		for (int j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				count += 1;
+				break;
+			}
+
+		}
+	}
+	return (count);
+}
