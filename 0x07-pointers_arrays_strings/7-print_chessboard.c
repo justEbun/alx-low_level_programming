c49fcdc58594b3057efa5234a384509942c9ef6c
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-26 16:53:16 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - printd a chessboard
+ * @a: chessboard
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	for (int i1 = 0; a[i1][7]; i1++)
+	{
+		for (int i2 = 0; i2 <8; i2++)
+		{
+			_putchar(a[i1][i2]);
+		}
+
+		_putchar('\n');
+	}
+}
