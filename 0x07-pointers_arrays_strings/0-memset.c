4ad01b704aa6104de1bec702a03f1f009e5ec52e
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-26 11:05:04 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_memset - fills the memory with a constant byte
+ * @s: pointer
+ * @b: the value to be filled
+ * @n: number of times
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (int i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
