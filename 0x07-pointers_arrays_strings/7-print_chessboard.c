8a6f17220ada9b429080c8f82e3dc433b36ede92
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-26 16:57:40 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,9 +7,11 @@
 
 void print_chessboard(char (*a)[8])
 {
-	for (int i1 = 0; a[i1][7]; i1++)
+	int i1, i2;
+
+	for (i1 = 0; a[i1][7]; i1++)
 	{
-		for (int i2 = 0; i2 < 8; i2++)
+		for (i2 = 0; i2 < 8; i2++)
 		{
 			_putchar(a[i1][i2]);
 		}
