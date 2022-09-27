a6bc6521a522a84ae08451ef75265ddf4a141380
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-27 14:20:23 -0700

    task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of two diagonals in a square matrix of integers
+ * @a: 2d array
+ * @size: number matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1, s2;
+
+	s1 = 0;
+	s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 = s1 + *(a + i * size + i);
+		s2 = s2 + *(a + i * size + size - i - 1);
+	}
+	_putchar(s1);
+	_putchar(s2);
+}
