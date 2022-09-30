2375cfba3cbe898a2d0667637efda2a52b2a6a2a
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-30 15:11:07 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,14 +14,15 @@ int main(int argc, char *argv[])
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
 	int result;
-	if (argc == 3)
+	
+	if (argc < 3)
+	{
+		printf("Error\n");
+	}
+	else if (argc == 3)
 	{
 		result = a * b;
 		printf("%d\n", result);
-	}
-	else
-	{
-		printf("Error\n");
 	}
 	return (0);
 }
