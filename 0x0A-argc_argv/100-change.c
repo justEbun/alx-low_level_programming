8124687580701e12d5d874e01fd187995f452ecf
Ebunoluwa oke <ebunoke18@gmail.com>
2022-09-30 19:41:31 -0700

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change for an amount of money.
+ * main - prints the minimum no of coins to make change for an amount of money
  * @argc: argument count
  * @argv: argument vector
  * Return: 0
