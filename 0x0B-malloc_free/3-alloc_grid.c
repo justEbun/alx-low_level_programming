2f60330b4f734e60527658424a017bdec81e4fd7
Ebunoluwa oke <ebunoke18@gmail.com>
2022-10-05 02:41:10 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	s = (int **)malloc(sizeof(int*) * height);
+	s = (int **)malloc(sizeof(int) * height);
 
 	for (i = 0; i < height; i++)
 	{
