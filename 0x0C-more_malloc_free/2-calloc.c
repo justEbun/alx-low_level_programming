d1d1a7e63e2a2d2cb6c98307da20ae45f72ff756
Ebunoluwa oke <ebunoke18@gmail.com>
2022-10-06 18:41:28 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,21 +11,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *s;
-	char *m;
+	char *s;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	s = size * nmemb;
+	s = malloc(size * nmemb);
 
 	if (s == NULL)
 		return (NULL);
-	m = s;
 
 	for (i = 0; i < size * nmemb; i++)
-		m[i] = '\0';
+		s[i] = 0;
 
 	return (s);
 }
