bf62e3529bf146e75c95c9490178c8da725b95fe
Ebunoluwa oke <ebunoke18@gmail.com>
2022-10-10 10:50:59 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include "dog.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: new dog info
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *bingo;
+	char *n, *o;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	bingo = malloc(sizeof(dog_t));
+
+	if (bingo == NULL)
+		return (NULL);
+
+	strcpy(n, name);
+	if (n == NULL)
+	{
+		free(bingo);
+		return (NULL);
+	}
+	(*bingo).name = n;
+
+	(*bingo).age = age;
+
+	strcpy(o, owner);
+	if (o == NULL)
+	{
+		free(bingo);
+		return (NULL);
+	}
+	(*bingo).owner = o;
+
+	return (bingo);
+}
