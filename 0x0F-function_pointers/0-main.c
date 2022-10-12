13655b7cf2dd5f78c8c89a8c47fe580ccb33def5
Ebunoluwa oke <ebunoke18@gmail.com>
2022-10-12 11:47:31 -0700

    task 0

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -10,7 +10,7 @@
 
 void print_name_as_is(char *name)
 {
-    printf("Hello, my name is %s\n", name);
+	printf("Hello, my name is %s\n", name);
 }
 
 /**
@@ -21,22 +21,22 @@ void print_name_as_is(char *name)
  */
 void print_name_uppercase(char *name)
 {
-    unsigned int i;
+	unsigned int i;
 
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
+	printf("Hello, my uppercase name is ");
+	i = 0;
+	while (name[i])
+	{
+		if (name[i] >= 'a' && name[i] <= 'z')
+		{
+			putchar(name[i] + 'A' - 'a');
+		}
+		else
+		{
+			putchar(name[i]);
+		}
+		i++;
+	}
 }
 
 /**
