bbb8efa67b5572c1d13edd9757ab99406dcbf13e
Ebunoluwa oke <ebunoke18@gmail.com>
2022-10-12 10:58:54 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointer.h"
+
+/**
+ * array_iterator - executes a function
+ * @array: array
+ * @size: size of array
+ * @action: pointer to the function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+	for (int i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
