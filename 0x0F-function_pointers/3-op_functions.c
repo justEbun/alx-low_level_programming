e4746393404630322a0ae99aac7032aab74a2652
Ebunoluwa oke <ebunoke18@gmail.com>
2022-10-12 21:07:33 -0700

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: integer value
+ * @b: integer value
+ * Return: addition
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - returns the diff. of a and b
+ * @a: integer value
+ * @b: integer value
+ * Return: subtraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - returns the product of a and b
+ * @a: integer value
+ * @b: integer value
+ * Return: multiplication
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - returns the division of a and b
+ * @a: integer value
+ * @b: integer value
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - returns the remainder of the division of a and b
+ * @a: integer value
+ * @b: integer value
+ * Return: modulus
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
