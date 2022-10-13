8316f6f0bf1e889a0d24c1e9bd1af8973c017ae2
Ebunoluwa oke <ebunoke18@gmail.com>
2022-10-12 21:21:08 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,8 +13,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	char c;
-	int (*func)(int, int);
+	char *c;
 
 	if (argc != 4)
 	{
@@ -24,20 +23,20 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	func = get_op_func(argv[2]);
+	c = argv[2];
 
-	if (!func)
+	if (!get_op_func(argv[2]) || c[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((c == '/' || c == '%') && num2 == 0)
+	if ((*c == '/' || *c == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	result = func(num1, num2);
+	result = get_op_func(c)(num1, num2);
 
 	printf("%d\n", result);
 	return (0);
