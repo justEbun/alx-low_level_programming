e5b08235139e80b77e570027efbd219fa623dd18
Ebunoluwa oke <ebunoke18@gmail.com>
2022-10-20 11:49:18 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: linked list_t list
+ * @elements: elements
+ * Return: the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
